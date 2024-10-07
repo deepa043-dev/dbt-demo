51b6aa8419de686e293a37fdfec34b65bd5c0816
deepa043@gmail.com <deepa043@gmail.com>
2024-10-07 01:46:33 +0000

    ver 1.0

--- models/example/stg_names2010.sql
+++ models/example/stg_names2010.sql
@@ -2,8 +2,10 @@ with stg_names2010 as (
     select
         name,
         concat(assigned_sex_at_birth,' Gender') as Gender,
-        count.
-        {{concat_test('name')}} as new_name
+        count,
+        {{concat_test('name')}} as new_name,
+        {{get_date_parts('2024-10-01')}} as new_name
+
     from babynames.names2010
 )
 
