7e0d2171065f251f514fd606195f6c2b8c483dd9
Deepa Joseph <deepa043@gmail.com>
2025-01-24 19:44:22 +0000

    changes

--- tests/no_orders_today.sql
+++ tests/no_orders_today.sql
@@ -1,6 +1,6 @@
 -- Test to check if there are no orders from today
 
 select count(*) as num_orders_today
-from {{ref('stg_orders')}}
+from {{ref('stage_orders')}}
 where DATE(order_date) = CURRENT_DATE()
 having count(*) >0
